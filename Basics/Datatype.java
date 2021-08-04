ca4973ff93afdba58f7e99987cc794683279e0f7
Anupam Dey <79352396+anupamdey2001@users.noreply.github.com>
2021-08-05 00:48:37 +0530

    Basics of java

---
+++ Basics/Datatype.java
@@ -0,0 +1,12 @@
+package com.company;
+public class Datatype{
+    public static void main(String[] args){
+        int roll = 11;
+        char c = 'c';
+        long largeInt = 123456789L;
+        float x = 113.5f;
+        double y = 123654.78885;
+        boolean z = false;
+//        System.out.println(char c);
+    }
+}
