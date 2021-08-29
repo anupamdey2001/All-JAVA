42ae654a73e1ff459ade045022e41177d118d373
Anupam Dey <79352396+anupamdey2001@users.noreply.github.com>
2021-08-29 13:02:49 +0530

    Add files via upload

---
+++ Bubble Sort/src/com/company/BubbleSort.java
@@ -0,0 +1,33 @@
+package com.company;
+
+import java.util.Arrays;
+
+public class BubbleSort {
+
+    public static void main(String[] args) {
+        int[] arr = {3,1,5,4,2};
+        bubble(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void bubble(int[] arr){
+        boolean swapped;//for performing O(N) complexity //For Best Case
+        //run the steps n-1 times
+        for (int i = 0; i < arr.length; i++) {
+            swapped = false;
+            //for each step,  max item will come at the last respective index
+            for (int j = 1; j < arr.length-i; j++) {
+                //swap if the item smaller than the previous item
+                if(arr[j] < arr[j-1]){
+                    //swap
+                    int temp = arr[j];
+                    arr[j] = arr[j-1];
+                    arr[j-1] = temp;
+                    swapped = true;
+                }
+            }
+            //if you did not swap for  a particular value of i, it means the array is sorted hence stop the program
+            if (!swapped)//!false = true
+            break;
+        }
+    }
+}
